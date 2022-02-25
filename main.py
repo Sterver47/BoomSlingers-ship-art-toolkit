43152c3173d832883ba93b660d5a78e845468cb8
Štefan Uram <stefan.uram@deftech.eu>
2022-02-25 20:48:24 +0100

    Update

--- main.py
+++ main.py
@@ -9,14 +9,20 @@ def main():
     print(art)
     print(art.generate_art_text_string())
 
-    art.generate_art_image_32x32(10).save("output/image.png")
+    art.make_art_image_32x32(20).save("output/image32x32.png")
+    art.make_art_image_60x24(20).save("output/image60x24.png")
 
 
 class Art:
-    __art_size = (32, 32)
+    __art_size_32x32 = (32, 32)
+    __art_size_50x24 = (60, 24)
+
     __art_text_string: str = None
     __raw_art_data: list[int] = None
     __chunked_raw_art_data: list[list[int]] = None
+
+    __chunked_big_art: list[list[int]] = None
+    __chunked_small_art: list[list[int]] = None
 
     def __init__(
         self,
@@ -38,6 +44,7 @@ class Art:
                 raise Exception("Not implemented.")
             else:
                 raise Exception("Can't generate data without any given data.")
+            self.split_chunked_data()
 
     def generate_raw_art_data(self) -> list[int]:
         compressed_data_from_string = base64.b64decode(self.__art_text_string)
@@ -65,18 +72,53 @@ class Art:
         self.__art_text_string = base64.b64encode(compressed).decode("ascii")
         return self.__art_text_string
 
-    def generate_art_image_32x32(self, scale: int = 1) -> Image:
-        image = Image.new("RGB", self.__art_size)
-        image.putdata([RGB_COLOURS[colour] for colour in self.__raw_art_data])
+    def split_chunked_data(self):
+        self.__chunked_big_art = self.__chunked_raw_art_data[8:]
+        self.__chunked_big_art.reverse()
+        self.__chunked_small_art = [
+            row[3:-1] for row in self.__chunked_raw_art_data[1:6]
+        ]
+        self.__chunked_small_art.reverse()
+        print(self.__chunked_small_art)
+
+    def make_art_image_32x32(self, scale: int = 1) -> Image:
+        size = self.__art_size_32x32
+        image = Image.new("RGB", size)
+        image.putdata([RGB_COLOURS[c_id] for c_id in self.__raw_art_data])
         if scale > 1:
             image = image.resize(
-                (self.__art_size[0] * scale, self.__art_size[1] * scale), resample=Image.BOX
+                (size[0] * scale, size[1] * scale),
+                resample=Image.BOX,
+            )
+        return image
+
+    def make_art_image_60x24(self, scale: int = 1) -> Image:
+        size = self.__art_size_50x24
+        image = Image.new("RGBA", size)
+
+        big_part = []
+        for row in self.__chunked_big_art:
+            big_part += row + [-1] * 28
+        image.putdata([RGB_COLOURS[c_id] for c_id in big_part])
+
+        small_image = Image.new("RGBA", size)
+        small_part = [-1]*60*19
+        for row in self.__chunked_small_art:
+            small_part += [-1] * 32 + row
+        small_image.putdata([RGB_COLOURS[c_id] for c_id in small_part])
+
+        image.paste(small_image, (0, 0), small_image)
+
+        if scale > 1:
+            image = image.resize(
+                (size[0] * scale, size[1] * scale),
+                resample=Image.BOX,
             )
         return image
 
     def __str__(self) -> str:
         pretty_string = ""
-        chunks = self.__chunked_raw_art_data
+        chunks = list(self.__chunked_raw_art_data)
         chunks.reverse()
         for row in chunks:
             for px in row:
@@ -91,24 +133,25 @@ class Art:
 # rgb_colours = [tuple(int(h[i+1:i+3], 16) for i in (0, 2, 4)) for h in hex_colours]
 
 
-RGB_COLOURS = [
-    (32, 34, 46),
-    (117, 13, 161),
-    (227, 69, 126),
-    (236, 174, 76),
-    (134, 169, 68),
-    (65, 125, 125),
-    (67, 82, 164),
-    (83, 46, 123),
-    (254, 254, 252),
-    (196, 126, 245),
-    (236, 128, 203),
-    (254, 246, 112),
-    (194, 251, 144),
-    (173, 247, 214),
-    (151, 204, 248),
-    (165, 126, 247),
-]
+RGB_COLOURS = {
+    -1: (0, 0, 0, 0),
+    0: (32, 34, 46),
+    1: (117, 13, 161),
+    2: (227, 69, 126),
+    3: (236, 174, 76),
+    4: (134, 169, 68),
+    5: (65, 125, 125),
+    6: (67, 82, 164),
+    7: (83, 46, 123),
+    8: (254, 254, 252),
+    9: (196, 126, 245),
+    10: (236, 128, 203),
+    11: (254, 246, 112),
+    12: (194, 251, 144),
+    13: (173, 247, 214),
+    14: (151, 204, 248),
+    15: (165, 126, 247),
+}
 
 
 if __name__ == "__main__":
