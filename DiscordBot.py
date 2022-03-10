2da24a2e49b9445d89f2f77cb4ce3c5f14419472
Štefan Uram <stefan.uram@deftech.eu>
2022-03-10 15:22:30 +0100

    Logging, better answers, etc.

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,10 +1,16 @@
 import io
+import logging
+import random
+
 import discord
 from Art import Art
 
+logger = logging.getLogger(__name__)
+
+
 class Bot(discord.Client):
     async def on_ready(self):
-        print("Everything's all ready to go~")
+        logger.info("Discord Bot is ready...")
 
     async def on_message(self, message):
         if message.author == self.user:
@@ -12,20 +18,32 @@ class Bot(discord.Client):
 
         content_string = str(message.content)
 
-        print("The message's content was", content_string)
-
         if "XQAAAQAAB" in content_string:
+            logger.info(f"XQAAAQAAB found in message by {message.author}: {content_string}")
+
             words = content_string.split()
+            art_strings = []
             for word in words:
                 if word.startswith("XQAAAQAAB"):
-                    print("LOG | art to convert: " + word)
+                    art_strings.append(word)
 
-                    art = Art(art_text_string=word)
+            if len(art_strings) > 2:
+                logger.info(f"{message.author} sent too many art codes in the message: {len(art_strings)}")
+                await message.channel.send("Hey! You've sent too many art codes in one message. My Creator decided that I can't convert that many art codes at once, so I will convert only the first two of them. I'm sincerely sorry for his bad decision. :pensive:")
+                art_strings = art_strings[:2]
 
+            for art_string in art_strings:
+                logger.info(f"Trying to convert '{art_string}' from {message.author}...")
+                try:
+                    art = Art(art_text_string=art_string)
                     with io.BytesIO() as image_data:
                         art.make_art_with_overlay().save(image_data, format="PNG")
                         image_data.seek(0)
                         file = discord.File(image_data, filename="art.png")
-
-                    await message.channel.send(str(message.author.mention)+"'s art", file=file)
+                    await message.channel.send(f"{str(message.author.mention)} sent this {random.choice(['great', 'fabulous', 'nice'])} art:\n(copyable code is below the image)", file=file)
+                    await message.channel.send(f"```{art_string}```")
                     file.close()
+                except Exception as e:
+                    await message.channel.send(f"Unfortunately, I struggled with converting the art code you shared to an image. Please, try to paste it into the in-game paint shop and if it works there, report it to my Creator: Sterver#0769. He probably messed something up in my code... :man_facepalming: The art code I failed to convert was:")
+                    await message.channel.send(f"```{art_string}```")
+                    logging.exception(e)
--- main.py
+++ main.py
@@ -1,36 +1,24 @@
-import io
-
-from Art import Art
-
-from PIL import Image
-
+import logging
+import sys
 from DiscordBot import Bot
+
+file_handler = logging.FileHandler(filename='tmp.log')
+stdout_handler = logging.StreamHandler(sys.stdout)
+handlers = [file_handler, stdout_handler]
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s] {%(name)s:%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
+    handlers=handlers
+)
+
+logger = logging.getLogger(__name__)
 
 
 def main():
-    # art_text_string = "XQAAAQAABAAAAAAAAAAAbh5RhjiWQJWnRyI9rtRTFHEIieb8urxGUYiJiIyU9+KHoIwF9T1Sw5De5ZSC5Q1/8SR303lFL5nIhy6qnWmTZBIHvr2bdE2ud4vxFgA="
-    # art_text_string = "XQAAAQAABAAAAAAAAAAAagCPB9wf3D0hwPHOSRp24fYRUj2Yyc4qWZjUPZ3Y1KFcqByiF13P4kVX2KIHuKl8SObmDnDfgV+TyV3hmizrUeCdmcHn6yRb469DkPmBai57vCmvmUWgTThPSxj4qM4w60756DP1Ep0l+RjImt/DBeX8FXdw05fgsQCXZyI9LpciC+jlEOTDlwnswTLvXUUV97iuoda2sTqoMedEg50Zm8uZISf0YMlS/ifL2aOohLzw8fLK/y2/67dWp0476ooxGIIEqn3wiI7uMbjmxokcafcUaFgRSVvOmt1nJZoNdUq5/PQ="
-    # art = Art(art_text_string=art_text_string)
-
-    # print(art.stringify())
-    # print(art)
-
-    # a = [0, 8, 2] * 341 + [0]
-    # art = Art(raw_art_data=a)
-    # print(art.stringify())
-    # print(art)
-
-    # art.make_art_image_32x32(20).save("output/image32x32.png")
-    # art.make_art_image_60x24(13).save("output/image60x24.png")
-
-    # artt = art.make_art_image_60x24(13)
-
-    # image = Image.new("RGBA", (984, 837))
-    # spaceship = Image.open("./spaceship.png")
-    # image.paste(artt, (130, 205), artt)
-    # image.save("./result.png")
-
+    logger.debug("Creating Bot...")
     bot = Bot()
+    logger.info("Starting Bot...")
     bot.run("OTQ3MTkwNTAwNDYzNjI0MjMy.Yhpp5Q.zsgiMRI5-faQdwJOvrNYe-xXoQU")
 
 
